b03d39a7e1ca115e2ae3a135092e9bb2d4ddc741
David Corcoran <44246360+ArcaneIRE@users.noreply.github.com>
2022-05-14 16:25:51 +0100

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    // Accept only numbers
+    if ((typeof num1 !== 'number') || (typeof num2 !== 'number')) return 'ERROR';
+    // Accept only positive numbers
+    if ((num1 < 0) || (num2 < 0)) return 'ERROR';
+    
+    // Uses sum of integers formula
+    if (num1 < num2) {
+        return ((num2 - num1 + 1) * (num1 + num2)) / 2;
+    } else {
+        return ((num1 - num2 + 1) * (num1 + num2)) / 2;
+    }
 };
 
 // Do not edit below this line
