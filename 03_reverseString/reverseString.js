3f49c3588f7fba57f6e05a81cae9459900379499
David Corcoran <44246360+ArcaneIRE@users.noreply.github.com>
2022-05-14 16:10:25 +0100

    Improve answer after viewing solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,11 +1,5 @@
 const reverseString = function(string) {
-    string = string.split('');
-    reversedString = '';
-
-    for (i = string.length - 1; i >= 0; i--) {
-        reversedString += string[i];
-    }
-    return reversedString;
+    return string.split('').reverse().join('');
 };
 
 // Do not edit below this line
