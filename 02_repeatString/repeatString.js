b3d07aab6395c15eee1f4317c83a3925c26b4f54
David Corcoran <44246360+ArcaneIRE@users.noreply.github.com>
2022-05-14 14:22:07 +0100

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const { TestScheduler } = require("jest");
 
+const repeatString = function(word='', repetitions=1) {
+    if (repetitions < 0) {
+        return 'ERROR';
+    }
+
+    let repeatedString = '';
+    for (i = 0; i < repetitions; i++) {
+        repeatedString += word;
+    }
+    return repeatedString
 };
 
 // Do not edit below this line
