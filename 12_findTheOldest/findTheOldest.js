32a279d4585d05271c5f19bf4b25e941d3400523
David Corcoran <44246360+ArcaneIRE@users.noreply.github.com>
2022-05-19 16:57:51 +0100

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.sort((a, b) => {
+        const currentYear = (new Date()).getFullYear();
 
+        let aAge = typeof a.yearOfDeath === 'number' ? 
+        a.yearOfDeath - a.yearOfBirth : currentYear - a.yearOfBirth;
+
+        let bAge = typeof b.yearOfDeath === 'number' ? 
+        b.yearOfDeath - b.yearOfBirth : currentYear - b.yearOfBirth;
+
+        return aAge > bAge ? 1 : -1;
+    }).at(-1);
 };
 
 // Do not edit below this line
