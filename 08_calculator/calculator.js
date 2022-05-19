641fac90b20f05e4f4ce079bd31eac91ef6583c6
David Corcoran <44246360+ArcaneIRE@users.noreply.github.com>
2022-05-19 14:21:02 +0100

    Finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => total += current, 0 );
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.slice(1).reduce((total, current) => total *= current, array[0] );
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let current = 1;
+	for (let i = 0; i < b; i++) {
+    current *= a;
+  }
+  return current; 
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a <= 1) {
+    return 1;
+  }
+  return a * factorial(a-1);
 };
 
 // Do not edit below this line
