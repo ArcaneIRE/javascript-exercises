a1480faef2b2b531372133aaad592946bbb62c60
David Corcoran <44246360+ArcaneIRE@users.noreply.github.com>
2022-05-19 15:17:06 +0100

    Improve exercise 10
    After viewing https://medium.com/@moulayjam/solving-the-fibonacci-algo-without-recursion-b1de3d94461d

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,7 +1,16 @@
 const fibonacci = function(n) {
     if (n < 0) return 'OOPS';
-    if (n == 1 || n == 2) return 1;
-    return fibonacci(n-2) + fibonacci(n-1);
+    a = 0;
+    b = 1;
+    let bCopy;
+
+    while (n > 0) {
+        bCopy = b;
+        b = a + b;
+        a = bCopy;
+        n--;
+    }
+    return a;
 };
 
 // Do not edit below this line
