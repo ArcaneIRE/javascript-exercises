eb8e729ca2f2410b6c5fe26cd15a2dafe903ee7a
David Corcoran <44246360+ArcaneIRE@users.noreply.github.com>
2022-05-19 14:58:30 +0100

    Finish exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+    string = string.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    let leftIndex = 0;
+    let rightIndex = string.length - 1;
+    do {
+        if (string[leftIndex] !== string[rightIndex]) {
+            return false;
+        }
+        leftIndex += 1;
+        rightIndex -= 1;
+    } while ((rightIndex - leftIndex) > 1);
+    return true;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
